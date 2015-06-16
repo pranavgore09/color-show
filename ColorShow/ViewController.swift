8c8d37795883f859dae0acab00bc1a3a5e0d98e6
Pranav Gore <pranavgore09@gmail.com>
2015-06-17 00:42:33 +0530

    initial

--- ColorShow/ViewController.swift
+++ ColorShow/ViewController.swift
@@ -8,8 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    let dataSource = ["red", "blue", "green", "black"]
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return dataSource.count
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return dataSource[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        var bgColor = UIColor()
+        switch row{
+        case 0:
+            bgColor = UIColor.redColor()
+        case 1:
+            bgColor = UIColor.blueColor()
+        case 2:
+            bgColor = UIColor.greenColor()
+        default:
+            bgColor = UIColor.blackColor()
+        }
+        self.view.backgroundColor = bgColor
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
